7d0f6f791da1241bdb3e7deb0b9cfe544b90a935
salvoM <salvowinter98@gmail.com>
2022-04-06 19:17:17 +0200

    Fixed testfile.c

--- user/testfile.c
+++ user/testfile.c
@@ -8,70 +8,104 @@
 
 #define SIZE 50
 
-void
-test0() {
-  // int i, j;
-  int fd;
-  char string[SIZE];
-  int res;
-  printf("[TEST] reading file descriptor: start\n");
-  // printf("Opening README.md");
-  if ((fd = open("echo", O_RDONLY)) < 0) {
-    // the open() failed; exit with -1
-    printf("open failed\n");
-    printf("[TEST] reading file descriptor: FAILED\n");
-    exit(-1);
-  }
-  else{
-    res = read( fd, string, SIZE-1);
-    if( res != 0){
-      string[SIZE-1] = 0;
-      printf("%s", string);
-      printf("Read %d bytes from %d\n", res, fd);
-    }
-    else{
-      printf("Cannot read from %d", fd);
-      printf("[TEST] reading file descriptor: FAILED\n\n\n");
-    }
-  }
-  printf("[TEST] reading file descriptor: OK\n\n\n");
+void test0()
+{
+	// int i, j;
+	int fd;
+	char string[SIZE];
+	int res;
+	printf("[READ TEST] START\n");
+	// printf("Opening README.md");
+	if ((fd = open("echo", O_RDONLY)) < 0)
+	{
+		// the open() failed; exit with -1
+		printf("[-] Open failed\n");
+		printf("[READ TEST] FAILED\n");
+		exit(-1);
+	}
+	else
+	{	
+		// File opened 
+		printf("[+] File opened\n");
+		res = read(fd, string, SIZE - 1);
+		if (res != 0)
+		{
+			string[SIZE - 1] = 0;
+			// printf("%s", string);
+			printf("[+] Read %d bytes from file descriptor %d\n", res, fd);
+		}
+		else
+		{
+			printf("[-] Cannot read from the opened file\n");
+			printf("[READ TEST] FAILED\n\n\n");
+			return;
+		}
+		close(fd);
+	}
+	printf("[READ TEST] SUCCESS\n\n\n");
 }
 
 void test1()
 {
-  // int pid, xstatus, n0, n;
-  int fd;
-  const char test_string[] = "This is a test string";
-  char buffer[SIZE];
+	/*
+	Il
+	*/
+	// int pid, xstatus, n0, n;
+	int fd, res = 0;
+	const char test_string[] = "This is a test string";
+	char buffer[SIZE];
+	const char filename[] = "dest_a";
+	printf("[WRITE TEST] START\n");
 
-  printf("[TEST] writing file descriptor: start\n");
-  
-  if ((fd = open("echo", O_RDWR)) < 0) {
-    printf("open failed\n");
-    printf("[TEST] writing file descriptor: FAILED\n");
-  }
-  else{
-    if( write(fd, test_string, sizeof(test_string)) != 0){
-      printf("write failed\n");
-      printf("[TEST] writing file descriptor: FAILED\n");
-      exit(-1);
-    }
-    else{
-      printf("Write: OK");
-      printf("Trying to read the content of the file...\n");
-      if( read(fd, buffer, sizeof(test_string)) == 0){
-        printf("Read failed\n");
-        printf("[TEST] writing file descriptor: FAILED\n");
-      }
-      printf("[TEST] writing file descriptor: OK\n");
-    }
-  }
+	if ((fd = open(filename, O_RDWR)) < 0)
+	{
+		printf("[-] Open failed\n");
+		printf("[-] Cannot open %s\n", filename);
+		printf("[WRITE TEST] FAILED\n");
+	}
+	else
+	{
+		// File opened
+		printf("[+] File opened\n");
+		if (write(fd, test_string, sizeof(test_string)) == 0)
+		{
+			printf("[-] Write failed\n");
+			printf("[WRITE TEST] FAILED\n");
+			close(fd);
+			exit(-1);
+		}
+		else
+		{
+			printf("[+] Write OK\n");
+			printf("[+] Trying to read the content of the file...\n");
+			
+			/*
+			You should not need to close and reopen the file,
+			but otherwise it does not work 
+			*/
+			close(fd);
+			fd = open(filename, O_RDONLY);
+			res = read(fd, buffer, sizeof(test_string));
+			printf("la stringa è %s", buffer);
+			if (res == 0)
+			{
+				printf("[-] Read failed\n");
+				printf("[WRITE TEST] FAILED\n");
+			}
+			else{
+				if (res == sizeof(test_string))
+					printf("[WRITE TEST] SUCCESS\n");
+				else
+					printf("[WRITE TEST] FAILED\n");
+			}
+		}
+		close(fd);
+	}
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  test0();
-  test1();
-  exit(0);
+	test0();
+	test1();
+	exit(0);
 }
